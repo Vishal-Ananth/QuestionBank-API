9d95cb1abbeffedfee91c66020ac03d52846cf31
Vishal Ananth J <imvishal2002@gmail.com>
2023-07-22 00:56:14 +0530

    createQuestion() and viewQuestion() functionality added

--- src/main/java/dev/vishal/questionbank/controller/QuestionController.java
+++ src/main/java/dev/vishal/questionbank/controller/QuestionController.java
@@ -1,15 +1,37 @@
 package dev.vishal.questionbank.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.vishal.questionbank.model.Question;
+import dev.vishal.questionbank.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("question")
 public class QuestionController {
 
-    @GetMapping("hello")
-    public String hello(){
-        return "Hello, World !";
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("all")
+    public List<Question> viewQuestions(){
+        return questionService.viewQuestions();
+    }
+
+//    @RequestMapping(method = RequestMethod.GET, produces = "/param",params = "topic")
+//    public List<Question> viewByTopic(@RequestParam String topic){
+//        return questionService.viewByTopic(topic);
+//    }
+
+//    @GetMapping("/difficulty")
+//    public List<Question> viewByDifficulty( String difficulty){
+//        return questionService.viewByDifficulty(difficulty);
+//    }
+
+    @PostMapping("add")
+    public String createQuestion(@RequestBody Question question){
+        return questionService.createQuestion(question);
     }
 }
--- src/main/java/dev/vishal/questionbank/service/QuestionService.java
+++ src/main/java/dev/vishal/questionbank/service/QuestionService.java
@@ -1,4 +1,32 @@
 package dev.vishal.questionbank.service;
 
+import dev.vishal.questionbank.dao.QuestionDao;
+import dev.vishal.questionbank.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public String createQuestion(Question question) {
+        questionDao.save(question);
+        return "Question "+question.getId()+" was created successfully!";
+    }
+
+    public List<Question> viewQuestions() {
+        return questionDao.findAll();
+    }
+
+    public List<Question> viewByTopic(String topic) {
+        return questionDao.findByTopic(topic);
+    }
+
+    public List<Question> viewByDifficulty(String difficulty) {
+        return questionDao.findByDifficulty(difficulty);
+    }
 }
