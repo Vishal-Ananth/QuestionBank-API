7c15ac4c268f6c74b4aff1acdfcf22d285ef84be
Vishal Ananth J <imvishal2002@gmail.com>
2023-07-21 20:56:40 +0530

    created appropriate packages

--- src/main/java/dev/vishal/questionbank/driver/QuestionbankApplication.java
+++ src/main/java/dev/vishal/questionbank/driver/QuestionbankApplication.java
@@ -1,4 +1,4 @@
-package dev.vishal.questionbank;
+package dev.vishal.questionbank.driver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
