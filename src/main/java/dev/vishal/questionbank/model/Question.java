2ccbb81f590dd62be766b13a75ff8d2266fc70f4
Vishal Ananth J <imvishal2002@gmail.com>
2023-07-21 21:42:07 +0530

    generated table in postgres

--- src/main/java/dev/vishal/questionbank/model/Question.java
+++ src/main/java/dev/vishal/questionbank/model/Question.java
@@ -1,11 +1,23 @@
 package dev.vishal.questionbank.model;
 
-public class Question {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
 
+@Entity
+@Data
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+    private String topic;
+    private String difficulty;
     private String optionA;
     private String optionB;
     private String optionC;
     private String optionD;
+    private String correctAnswer;
 }
