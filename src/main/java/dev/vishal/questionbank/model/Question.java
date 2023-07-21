1dcc8db4564bb4b8c6e4b98ae7ed86bd6d3e3303
Vishal Ananth J <imvishal2002@gmail.com>
2023-07-21 21:36:35 +0530

    refactored QuestionbankApplication.java

--- src/main/java/dev/vishal/questionbank/model/Question.java
+++ src/main/java/dev/vishal/questionbank/model/Question.java
@@ -1,4 +1,11 @@
 package dev.vishal.questionbank.model;
 
 public class Question {
+
+    private Integer id;
+    private String name;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
 }
